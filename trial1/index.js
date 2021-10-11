50cc639e33c3c477f89bcb87a2335f852c066e28
Aditya-013 <33128013+Aditya-013@users.noreply.github.com>
2021-10-11 13:36:34 +0530

    Add files via upload

---
+++ trial1/index.js
@@ -0,0 +1,16 @@
+runThis = () =>{
+    console.log("Running This");
+}
+
+setTimeout(runThis, 2000);
+
+console.log('Hey mate!')
+
+let el = document.querySelector("#ID1");
+console.log(el)
+
+const pageTitle = document.querySelector('#QuerrySelector1');
+const PageTitleAgain = document.getElementById('QuerrySelector1');
+
+console.log(pageTitle);
+console.log(PageTitleAgain);
